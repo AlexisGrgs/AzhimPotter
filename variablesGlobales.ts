0791150545257976f8112b64a0703a8c3b14cb51
AlexisGrgs <alexol98@hotmail.fr>
2020-03-02 22:33:55 +0100

    adding global variables

---
+++ variablesGlobales.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+@Injectable()
+export class VariablesGlobales {
+  sorcier = 'Azhim';
+  bookPictureUrl = 'https://pbs.twimg.com/profile_images/791642012936200192/hOA2XMnZ_400x400.jpg';
+  competences = [0,0,0,0,0,0,0,0,0];
+}
